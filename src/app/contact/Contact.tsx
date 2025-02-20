370639b02e5e3853584c49cbd27a0bafa10a7215
kevinmuchene <17735347+kevinmuchene@users.noreply.github.com>
2025-02-20 15:53:17 -0600

    Added a general inquiries section

--- src/app/contact/Contact.tsx
+++ src/app/contact/Contact.tsx
@@ -1,18 +1,19 @@
+import MapComponent from "@/components/MapComponent";
 import React from "react";
 
 function Contact() {
   return (
     <>
-      <p className="text-xl md:text-4xl text-center">Contact</p>
-      <div className="min-h-full flex items-center justify-center p-6 bg-white">
+      <p className="text-xl md:text-4xl text-center font-extrabold">Contact</p>
+      <div className=" flex items-center justify-center p-6 bg-white">
         <main className="flex flex-col gap-7 md:flex-row items-center space-x-8 max-w-[120ch] mx-auto w-full justify-around">
-          <section className="flex items-center justify-center">
-            Map
+          <section className="flex items-center justify-center w-full">
+            <MapComponent />
           </section>
 
           <section className=" md:text-lg space-y-2">
             <p>(123) 456 7890</p>
-            <p>email@gmail.com</p>
+          
             <br />
             <p>
               Concordia University Texas, <br />
@@ -24,6 +25,12 @@ function Contact() {
           </section>
         </main>
       </div>
+
+      <section className="md: p-7">
+        <p className="text-center text-xl md:text-3xl mb-4 md:mb-6 font-extrabold"> Email</p>
+        <p className="text-center p-2 md:text-lg">General Inquiries</p>
+        <p className="text-center p-2 italic text-blue-500">email@gmail.com</p>
+      </section>
     </>
   );
 }
