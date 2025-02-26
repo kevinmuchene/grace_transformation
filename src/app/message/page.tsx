bd2bb85161edccbdc7adf54578a88e09ad075bbd
kevinmuchene <17735347+kevinmuchene@users.noreply.github.com>
2025-02-26 10:50:51 -0600

    Added image in the message landing

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -2,12 +2,24 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
+import Image from "next/image";
+import asyncImage from "../../../public/asyncImage.webp";
 
 const NoSSR = dynamic(() => import("./MessageLandingPage"), { ssr: false });
 
 function page() {
   return (
     <>
+      <div className="flex justify-center">
+        <Image
+          src={asyncImage}
+          alt="Async Programming Image"
+          style={{
+            maxWidth: "40%",
+            height: "auto",
+          }}
+        />
+      </div>
       <NoSSR />
     </>
   );
