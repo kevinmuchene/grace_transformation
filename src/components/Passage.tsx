2544e5655ca6aa12cfe34d300d941c900e132542
kevinmuchene <17735347+kevinmuchene@users.noreply.github.com>
2025-03-03 11:30:23 -0600

    Added some padding around the passage component

--- src/components/Passage.tsx
+++ src/components/Passage.tsx
@@ -20,9 +20,9 @@ const Passage = ({
   }
 
   return (
-    <div className="p-3">
+    <div className="pt-2 pr-4 pb-7 pl-4 md:pr-15 md:pb-15 md:pl-15 md:pt-7 rounded-xl shadow-2xl bg-white">
       <main className="flex justify-end p-3">
-        <section className="flex items-center justify-center gap-3 bg-white shadow-md rounded-2xl p-4 max-w-md">
+        <section className="flex items-center justify-center gap-3 bg-slate-100 shadow-md rounded-2xl p-4 max-w-md">
           <h2 className="text-sm md:text-lg">Select Bible Version:</h2>
           <select
             className="px-4 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition bg-white"
