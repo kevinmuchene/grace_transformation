5cc55e8a10c78a335122adc7bf954aadc0ff0ed6
kevinmuchene <17735347+kevinmuchene@users.noreply.github.com>
2025-03-07 21:04:22 -0600

    Added Days and Months enums

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,30 @@ export interface ApiContent {
   copyright: string;
   fumsJs: string;
 }
+
+export enum Days {
+    Sunday = "Sunday",
+    Monday = "Monday",
+    Tuesday = "Tuesday",
+    Wednesday = "Wednesday",
+    Thursday = "Thursday",
+    Friday = "Friday",
+    Saturday = "Saturday"
+  }
+  
+  export enum Months {
+    January = "January",
+    February = "February",
+    March = "March",
+    April = "April",
+    May = "May",
+    June = "June",
+    July = "July",
+    August = "August",
+    September = "September",
+    October = "October",
+    November = "November",
+    December = "December"
+  }
+  
+  
